6f4ae0fd4ccfea5f6f249521e4e8add24c3e171d
nabeillemar <55730283+nabeillemar@users.noreply.github.com>
2022-10-19 11:34:34 -0700

    Add files via upload

---
+++ header.js
@@ -0,0 +1,50 @@
+class Header extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+connectedCallback() {
+    this.innerHTML = `
+    <style>
+    /* Style the top navigation bar */
+    .navbar {
+    overflow: hidden;
+    background-color: #333;
+    }
+  
+    /* Style the navigation bar links */
+    .navbar a {
+    float: left;
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 14px 20px; 
+    text-decoration: none;
+    }
+  
+    /* Right-aligned link */
+    .navbar a.right {
+    float: right;
+    }
+  
+    /* Change color on hover */
+    .navbar a:hover {
+    /*background-color: rgb(93, 194, 11);*/
+
+    }
+    </style>
+    
+    <header>
+      <div class="navbar">
+      <a href="index.html"><i class="fa-solid fa-house"></i> Home</a>
+      <a href="about.html"><i class="fa-solid fa-school-flag"></i>  Activites </a>
+      <a href="contact-us.html"><i class="fa-solid fa-envelope"></i> Contact </a> 
+      <a href="#" class="right">Link</a>
+     </div>
+    </header>
+  `;
+}
+
+}
+
+customElements.define('header-component', Header);
